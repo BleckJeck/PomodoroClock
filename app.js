a4c4fe820a7c333d516862490462ad8b45a18b55
Giacomo Carnovalini <giacomo.carnovalini@gmail.com>
2017-10-14 19:19:41 +0100

    different colour cycles, input disabled when running

--- app.js
+++ app.js
@@ -13,37 +13,38 @@ let sec = document.getElementById('sec');
 
 // adds event listeners to the buttons
 play.addEventListener('click', setTimer);
-play.addEventListener('click', startCountdown);
 pause.addEventListener('click', stopCountdown);
 
 // the timer needs to a global variable to be accessed by clearInterval
 let timer;
 let colourFill;
 
-let seconds;
 let minutes;
+let seconds;
 
 let step;
 let deg;
 
-let activeCycle = true;
+let activeCycle;
 
 function setTimer() {
   // grabs the input values
   minutes = document.getElementById('work').value;
   seconds = 0;
-  deg = 0;
+
   step = minutes * 60 / 360;
+  deg = 0;
+
+  activeCycle = true;
+
   // sets the starting time on the clock
   min.innerHTML = minutes.toString().padStart(2, '0');
   sec.innerHTML = seconds.toString().padStart(2, '0');
-}
 
-function stopCountdown() {
-  clearInterval(timer);
-  clearInterval(colourFill);
-  play.style.display = 'block';
-  pause.style.display = 'none';
+  outer.style.backgroundImage = 'linear-gradient(' + (deg+90) +'deg, transparent 50%, white 50%), linear-gradient(90deg, white 50%, red 50%)';
+  deg++;
+
+  startCountdown();
 }
 
 function startCountdown() {
@@ -51,6 +52,17 @@ function startCountdown() {
   colourFill = setInterval(fillArea, step*1000);
   play.style.display = 'none';
   pause.style.display = 'block';
+  document.getElementById('work').setAttribute("disabled","disabled");
+  document.getElementById('break').setAttribute("disabled","disabled");
+}
+
+function stopCountdown() {
+  clearInterval(timer);
+  clearInterval(colourFill);
+  play.style.display = 'block';
+  pause.style.display = 'none';
+  document.getElementById('work').removeAttribute("disabled");
+  document.getElementById('break').removeAttribute("disabled");
 }
 
 function countdown() {
@@ -81,15 +93,29 @@ function countdown() {
 }
 
 function fillArea() {
-  if (deg > 360) {
-    clearInterval(colourFill);
-    deg = 0;
-  }
-  else if (deg <= 180) {
-    outer.style.backgroundImage = 'linear-gradient(' + (deg+90) +'deg, transparent 50%, white 50%), linear-gradient(90deg, white 50%, red 50%)';
-    deg++;
+  if (activeCycle) {
+    if (deg > 360) {
+      clearInterval(colourFill);
+      deg = 0;
+    }
+    else if (deg <= 180) {
+      outer.style.backgroundImage = 'linear-gradient(' + (deg+90) +'deg, transparent 50%, white 50%), linear-gradient(90deg, white 50%, green 50%)';
+      deg++;
+    } else {
+      outer.style.backgroundImage = 'linear-gradient(' + (deg+90) +'deg, green 50%, transparent 50%), linear-gradient(90deg, white 50%, green 50%)';
+      deg++;
+    }
   } else {
-    outer.style.backgroundImage = 'linear-gradient(' + (deg+90) +'deg, red 50%, transparent 50%), linear-gradient(90deg, white 50%, red 50%)';
-    deg++;
+    if (deg > 360) {
+      clearInterval(colourFill);
+      deg = 0;
+    }
+    else if (deg <= 180) {
+      outer.style.backgroundImage = 'linear-gradient(' + (deg+90) +'deg, transparent 50%, white 50%), linear-gradient(90deg, white 50%, red 50%)';
+      deg++;
+    } else {
+      outer.style.backgroundImage = 'linear-gradient(' + (deg+90) +'deg, red 50%, transparent 50%), linear-gradient(90deg, white 50%, red 50%)';
+      deg++;
+    }
   }
 }
