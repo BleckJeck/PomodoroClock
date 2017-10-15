e518d4d6ba9106912e1466396a04f477cdf6afa2
Giacomo Carnovalini <giacomo.carnovalini@gmail.com>
2017-10-15 16:09:36 +0100

    added comments

--- app.js
+++ app.js
@@ -7,31 +7,42 @@ const pause = document.getElementById('pause');
 // grabs the outer circle for colour fill animation
 const outerCircle = document.getElementById('outer');
 
-// grabs the minutes and seconds on the clock
+// grabs the minutes and seconds displayed in the clock
 let min = document.getElementById('min');
 let sec = document.getElementById('sec');
 
-// adds event listeners to the buttons
+// adds event listeners to the play and pause buttons
 play.addEventListener('click', setTimer);
 pause.addEventListener('click', stopCountdown);
 
-// the timer needs to a global variable to be accessed by clearInterval
+/*
+Timer variables
+The timers needs to be set globally to be accessed by clearInterval
+*/
 let timer;
 let colourFill;
 
+// minutes selected in the input
 let minutes;
 let seconds;
 
+// seconds needed to advance the colourFill animation by 1 degree
 let step;
+
+// current degrees for the colourFill animation
 let deg;
 
+// keeps track of the cycle we're on (work or break)
 let activeCycle;
 
 function setTimer() {
-  // grabs the input values
+  // resets all values to their initial status
+
+  // sets minutes as the 'work' time
   minutes = document.getElementById('work').value;
   seconds = 0;
 
+  // calculates the step for the 'work' time
   step = minutes * 60 / 360;
   deg = 0;
 
@@ -41,6 +52,7 @@ function setTimer() {
   min.innerHTML = minutes.toString().padStart(2, '0');
   sec.innerHTML = seconds.toString().padStart(2, '0');
 
+  // reset the colourFill animation to default
   outer.style.backgroundImage = 'linear-gradient(' + (deg+90) +'deg, transparent 50%, #123776 50%), linear-gradient(90deg, #123776 50%, #4F8438 50%)';
   deg++;
 
@@ -48,15 +60,21 @@ function setTimer() {
 }
 
 function startCountdown() {
+  // decreases the time every 1 second
   timer = setInterval(countdown, 1000);
+
+  // advances the colourFill animation by 1 deg every 'step' sec
   colourFill = setInterval(fillArea, step*1000);
   play.style.display = 'none';
   pause.style.display = 'block';
+
+  // disables the inputs during when the clock is ticking
   document.getElementById('work').setAttribute("disabled","disabled");
   document.getElementById('break').setAttribute("disabled","disabled");
 }
 
 function stopCountdown() {
+  // stops the timer and re-enables the inputs
   clearInterval(timer);
   clearInterval(colourFill);
   play.style.display = 'block';
@@ -74,7 +92,7 @@ function countdown() {
     minutes--;
     sec.innerHTML = seconds.toString().padStart(2, '0');
     min.innerHTML = minutes.toString().padStart(2, '0');
-  } else if (sec.innerHTML == 0 && min.innerHTML == 0) {
+  } else if (sec.innerHTML == 0 && min.innerHTML == 0) { // end of cycle
     clearInterval(timer);
     clearInterval(colourFill);
     activeCycle = !activeCycle;
